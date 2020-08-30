f3a83c0b4401be9c1db449233c93080452b0ae7c
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-08-30 18:05:16 -0400

    Fix models

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Invoice, type: :model do
   describe 'validations' do
-    it { should_validate_presence_of :status }
+    it { should validate_presence_of :status }
   end
 
   describe 'relationships' do
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 describe Item, type: :model do
   describe 'validations' do
-    it { should_validate_presence_of :name }
-    it { should_validate_presence_of :description }
-    it { should_validate_presence_of :unit_price }
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :unit_price }
   end
 
   describe 'relationships' do
