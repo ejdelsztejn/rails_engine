4824a74395b4ebf575b5f6dfda6e0266dabb0b8e
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-08-30 16:39:14 -0400

    Add transactions

--- db/migrate/20200830203726_create_transactions.rb
+++ db/migrate/20200830203726_create_transactions.rb
@@ -1,6 +1,11 @@
 class CreateTransactions < ActiveRecord::Migration[5.2]
   def change
     create_table :transactions do |t|
+      t.references :invoice
+      t.string :credit_card_number
+      t.string :result
+
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_30_203252) do
+ActiveRecord::Schema.define(version: 2020_08_30_203726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,15 @@ ActiveRecord::Schema.define(version: 2020_08_30_203252) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.bigint "invoice_id"
+    t.string "credit_card_number"
+    t.string "result"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
   end
 
   add_foreign_key "invoice_items", "invoices"
