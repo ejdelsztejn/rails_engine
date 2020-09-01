4c9a4cd72fd7927079f6bef0e1c9df0b8942b537
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-01 07:15:09 -0400

    Add forgotten 'do' blocks'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :api
-    namespace :v1
+  namespace :api do
+    namespace :v1 do
       resources :items, only: [:index]
+    end
+  end
 end
