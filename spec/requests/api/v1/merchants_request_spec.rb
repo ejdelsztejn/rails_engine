0e1a4ea4e90dbc3f52aac44eb7f91ece51181da7
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-01 15:46:09 -0400

    Create merchants request spec

---
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'Merchants Endpoints' do
+  it 'can return a list of all merchants' do
+    create_list(:merchant, 5)
+
+    get '/api/v1/merchants'
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants[:data].count).to eq(5)
+    expect(items[:data].first).to have_key(:id)
+    expect(items[:data].first).to have_key(:type)
+    expect(items[:data].first).to have_key(:attributes)
+  end
+end
