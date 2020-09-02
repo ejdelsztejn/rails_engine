b050816adfebbd1f5678ba04e2ade370d081c070
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-02 18:07:25 -0400

    Add find_all_merchants method to Merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -7,7 +7,15 @@ class Merchant < ApplicationRecord
     if attribute == 'created_at' || attribute == 'updated_at'
       Merchant.find_by("to_char(#{attribute},'yyyy-mon-dd-HH-MI-SS') ILIKE ?", "%#{value}%")
     else
-      Merchant.find_by("#{attribute} ILIKE ?", "%#{value}%")
+      Merchant.find_by("merchants.#{attribute} ILIKE ?", "%#{value}%")
+    end
+  end
+
+  def self.find_all_merchants(attribute, value)
+    if attribute == 'created_at' || attribute == 'updated_at'
+      Merchant.where("to_char(#{attribute},'yyyy-mon-dd-HH-MI-SS') ILIKE ?", "%#{value}%")
+    else
+      Merchant.where("merchants.#{attribute}::text ILIKE ?", "%#{value}%")
     end
   end
 end
