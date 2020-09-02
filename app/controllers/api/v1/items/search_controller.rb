6b935dfa849dc6a14ef017dd8868b158f76e43b6
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-02 17:49:08 -0400

    Add index method

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -3,6 +3,12 @@ class Api::V1::Items::SearchController < ApplicationController
     attribute, value = query_params.to_h.first
     render json: ItemSerializer.new(Item.find_item(attribute, value))
   end
+
+  def index
+    attribute, value = query_params.to_h.first
+    render json: ItemSerializer.new(Item.find_all_items(attribute, value))
+  end
+
   private
 
   def query_params
