380c24201797f886be05b82ac9372db80a692f9a
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-01 20:41:29 -0400

    Add additional expectations

--- spec/requests/api/v1/items_merchant_request_spec.rb
+++ spec/requests/api/v1/items_merchant_request_spec.rb
@@ -10,10 +10,10 @@ describe 'Items Merchant Endpoint' do
 
     item_merchant = JSON.parse(response.body, symbolize_names: true)
 
-    expect(item_merchant[:data].count).to eq(1)
-    expect(item_merchant[:data].first).to have_key(:id)
-    expect(item_merchant[:data].first).to have_key(:type)
-    expect(item_merchant[:data].first).to have_key(:attributes)
-    expect(item_merchant[:data].first[:attributes][:name]).to eq('Lizard Palace')
+    expect(item_merchant[:data]).to have_key(:id)
+    expect(item_merchant[:data]).to have_key(:type)
+    expect(item_merchant[:data]).to have_key(:attributes)
+    expect(item_merchant[:data][:type]).to eq('merchant')
+    expect(item_merchant[:data][:attributes][:name]).to eq('Lizard Palace')
   end
 end
