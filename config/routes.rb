ff24ce19910511ab1882755d78719fce1eb029f0
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-01 20:35:01 -0400

    Fix item merchant route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :items, only: [:index, :show, :create, :update, :destroy] do
-        resources :merchants, only: [:show], module: :items
+        resources :merchant, only: [:index], module: :items
       end
       resources :merchants, only: [:index, :show, :create, :update, :destroy] do
         resources :items, only: [:index], module: :merchants
