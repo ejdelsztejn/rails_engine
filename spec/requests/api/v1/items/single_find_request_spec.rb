7d837b1a438207899cd44497d5654baf6b86173a
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-01 22:54:22 -0400

    Add single finder items spec

---
+++ spec/requests/api/v1/items/single_find_request_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'Items Single Finder Endpoint' do
+  it 'can return a single record that matches a set of criteria' do
+    merchant = create(:merchant)
+    item = Item.create(name: "Kangaroo", description: 'A little friend', unit_price: 1.50, merchant_id: merchant.id)
+    item = Item.create(name: "Sloth", description: 'A little friend', unit_price: 1.50, merchant_id: merchant.id)
+    item = Item.create(name: "Crocodile", description: 'A little friend', unit_price: 1.50, merchant_id: merchant.id)
+
+    get "/api/v1/items/find?name=sloth"
+    expect(response).to be_successful
+
+    item = JSON.parse(response.body, symbolize_names: true)
+
+    expect(item[:data][:type]).to eq('item')
+    expect(item[:data][:attributes][:name]).to eq('Sloth')
+  end
+end
