d34a4f9a7ed22fe1b93544bd6632735f35a9a6cb
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-02 09:36:44 -0400

    Add query_params strong params

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def show
+    attribute, value = query_params.to_h.first
     require "pry"; binding.pry
+  end
+
+  def query_params
+    params.permit(:name, :created_at, :updated_at)
   end
 end
