e7433c7fbf0226a8d5db645690deb53eb234334f
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-02 09:31:41 -0400

    Add merchants single finder endpoint route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
       namespace :items do
         get '/find', to: 'search#show'
       end
+      namespace :merchants do
+        get '/find', to: 'search#show'
+      end
       resources :merchants, only: [:index, :show, :create, :update, :destroy] do
         resources :items, only: [:index], module: :merchants
       end
