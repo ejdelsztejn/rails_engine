51f1f94643fc364b4b24fefba28b273bc5df7902
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-02 09:11:57 -0400

    Add strong params query_params

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,5 +1,12 @@
 class Api::V1::Items::SearchController < ApplicationController
   def show
     require "pry"; binding.pry
+    attribute, value = query_params.to_h.first
+  end
+
+  private
+
+  def query_params
+    params.permit(:name, :unit_price, :description, :created_at, :updated_at, :merchant_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      get 'items/find', to: 'search#show'
-      resources :items do
-        # get ':search(/:action(/:id))'
-        get '/items', to: 'items#index'
-        get '/items/:id', to: 'items#show'
-        post '/items', to: 'items#create'
-        patch '/items/:id', to: 'items#update'
-        delete '/items/:id', to: 'items#destroy'
-        resources :merchant, only: [:index], module: :items
+      namespace :items do
+        get '/find', to: 'search#show'
       end
       resources :merchants, only: [:index, :show, :create, :update, :destroy] do
         resources :items, only: [:index], module: :merchants
+      end
+      resources :items, only: [:index, :show, :create, :update, :destroy] do
+        resources :merchant, only: [:index], module: :items
       end
     end
   end
