6b9b1ef44330f4d12ff02f86c81161efa0d7772f
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-02 22:24:47 -0400

    Add route for merchant revenue

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
         get '/find', to: 'search#show'
         get '/find_all', to: 'search#index'
         get '/most_revenue', to: 'revenue#most_revenue'
+        get '/:id/revenue', to: 'revenue#revenue'
       end
       resources :merchants, only: [:index, :show, :create, :update, :destroy] do
         resources :items, only: [:index], module: :merchants
