fb967a63394837b53da90d8d63b5006590b1b855
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-02 20:38:56 -0400

    Add find_merchants method

--- app/facades/revenue_facade.rb
+++ app/facades/revenue_facade.rb
@@ -1,2 +1,8 @@
 class RevenueFacade
+  def find_merchants(quantity)
+    merchant_revenue = get_merchants_revenue(quantity)
+    merchant_revenue.each do |revenue|
+      Revenue.new(revenue)
+    end
+  end
 end
