c0f9f10ff124cc1bace5a1f54535ecc69d3f61a6
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-02 20:25:05 -0400

    Add quantity_params; map out plan in comments

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,2 +1,20 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
+  def most_revenue
+    quantity = quantity_params[:quantity]
+    require "pry"; binding.pry
+    # create RevenueFacade
+    # create Revenue PORO
+    # create RevenueSerializer
+
+    # within RevenueFacade, create find_merchants method
+    # within the find_merchants method, call a get_merchants_revenue method with ActiveRecord query
+    # using data from get_merchants_revenue, iterate through and create a new instance of a Revenue PORO
+    # within this method, render json with the RevenueSerializer and a new instance of the RevenueFacade + find_merchants
+  end
+
+  private
+
+  def quantity_params
+    params.permit(:quantity)
+  end
 end
